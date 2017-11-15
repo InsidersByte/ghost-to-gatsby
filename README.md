85f83790e6fbc989742cfcbd2a74a7e636d367f3
Jonathon Kelly <jonathon@insidersbyte.com>
2017-11-15 19:27:40 +0100

    :sparkles: Tags (#1)

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # ghost-to-gatsby
 
-:construction: A CLI to convert a ghost backup.json to gatsby pages. :construction:
+:construction: A CLI to convert a ghost backup.json to gatsby pages.
+:construction:
 
 ## Progress
 
-- [X] Create basic pages
-- [ ] Expose as CLI
-- [ ] Add tags
-- [ ] Grab all images
+* [x] Create basic pages
+* [ ] Expose as CLI
+* [ ] Publish to npm to use with npx
+* [X] Add tags
+* [ ] Grab all images
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import path from 'path';
 import json from '../example/insiders-byte.ghost.2017-11-15.json';
 import type { RootInterface } from './types';
 
-type ProcessedPost = {
+type Post = {
   path: string,
   date: moment,
   title: string,
@@ -14,7 +14,7 @@ type ProcessedPost = {
   tags: string[],
 };
 
-type ProcessedPosts = ProcessedPost[];
+type Posts = Post[];
 
 const BASE_PATH = './pages';
 const root: RootInterface = json;
@@ -26,30 +26,46 @@ if (dbs.length !== 1) {
 
 const db = dbs[0];
 const { data } = db;
-const { posts } = data;
 
-const processedPosts: ProcessedPosts = posts.map(({ title, slug, markdown, published_at }) => ({
-  path: `/${slug}`,
-  date: moment(published_at),
-  title,
-  markdown,
-  tags: [],
-}));
+const posts: Posts = data.posts.map(
+  ({ title, slug, markdown, published_at, id }) => {
+    const postTags = data.posts_tags
+      .filter(o => o.post_id === id)
+      .sort((a, b) => a.sort_order - b.sort_order)
+      .map(o => o.tag_id);
+
+    const tags = data.tags.filter(o => postTags.includes(o.id)).map(o => o.name );
+
+    return {
+      path: `/${slug}`,
+      date: moment(published_at),
+      title,
+      markdown,
+      tags,
+    };
+  },
+);
 
 fs.mkdirSync(BASE_PATH);
 
-processedPosts.forEach((post: ProcessedPost) => {
-  const basePath = path.join(BASE_PATH, `${post.date.format("YYYY-MM-DD")}-${post.title}`);
-  const markdownPath = path.join(basePath, "index.md");
+posts.forEach(({ date, path: postPath, title, tags, markdown }: Post) => {
+  const basePath = path.join(
+    BASE_PATH,
+    `${date.format('YYYY-MM-DD')}-${title}`,
+  );
+  const markdownPath = path.join(basePath, 'index.md');
 
   fs.mkdirSync(basePath);
-  fs.writeFileSync(markdownPath, `---
-path: "${post.path}"
-date: "${post.date.toISOString()}"
-title: "${post.title}"
-tags: []
+  fs.writeFileSync(
+    markdownPath,
+    `---
+path: "${postPath}"
+date: "${date.toISOString()}"
+title: "${title}"
+tags: ${JSON.stringify(tags)}
 ---
 
-${post.markdown}
-  `)
+${markdown}
+  `,
+  );
 });
